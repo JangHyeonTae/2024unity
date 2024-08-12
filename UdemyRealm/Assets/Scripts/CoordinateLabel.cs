79300f1e73cd953c0517bffc51eba943f83a7925
jht <wodnd565@naver.com>
2024-08-12 16:52:53 +0900

    realmrush
    .

---
+++ UdemyRealm/Assets/Scripts/CoordinateLabel.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+[ExecuteAlways]
+[RequireComponent(typeof(TextMeshPro))]
+public class CoordinateLabel : MonoBehaviour
+{
+    [SerializeField] Color defaultColor = Color.white;
+    [SerializeField] Color blockedColor = Color.gray;
+
+    TextMeshPro label;
+    Vector2Int coordinates = new Vector2Int();
+    WayPoint waypoint;
+
+    void Awake()
+    {
+        label = GetComponent<TextMeshPro>();
+        label.enabled = false;
+
+        waypoint = GetComponentInParent<WayPoint>();
+        DisPlayCoordinates();
+    }
+    void Update()
+    {
+        if (!Application.isPlaying)
+        {
+            DisPlayCoordinates();
+            UpdateObjectName();
+            label.enabled = false;
+        }
+
+        SetLabelColor();
+        ToggleLabels();
+    }
+
+    void ToggleLabels()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            label.enabled = !label.IsActive();
+        }
+    }
+
+    void SetLabelColor()
+    {
+        if(waypoint.IsPlaceable)
+        {
+            label.color = defaultColor;
+        }
+        else
+        {
+            label.color = blockedColor;
+        }
+    }
+
+    void DisPlayCoordinates()
+    {
+        coordinates.x = Mathf.RoundToInt(transform.parent.position.x / UnityEditor.EditorSnapSettings.move.x);
+        coordinates.y = Mathf.RoundToInt(transform.parent.position.z / UnityEditor.EditorSnapSettings.move.z);
+
+        label.text = coordinates.x + "," + coordinates.y;
+    }
+
+    void UpdateObjectName()
+    {
+        transform.parent.name = coordinates.ToString();
+    }
+}
